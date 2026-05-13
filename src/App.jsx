f8f7c25ed987cff3c0f1bed5ad8cd2f34f64a3e7
DarshanSivasubramaniam <s.darshan252007@gmail.com>
2026-05-13 10:09:31 +0530

    Update header name from 'sdarshan36' to 'Darshan S'

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>sdarshan36</h1>
+          <h1>Darshan S</h1>
           <p>
           <code>CI CD</code>
           </p>
